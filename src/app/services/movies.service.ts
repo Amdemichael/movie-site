44623680971faeb7c9c3de21ea3f73b27e7cd87e
Amdemichael Kidane <amkidane@excellerentsolutions.com>
2023-05-14 13:09:36 +0300

    Movie-site initial application shell

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root',
+})
+export class MoviesService {
+  constructor(private http: HttpClient) {}
+
+  getMovies() {
+    return this.http.get(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=52e3200582daea5c719f691e2e943b30'
+    );
+  }
+}
